d14317fb9d3c3cff6d166f0c43a62289741ddf61
Marcello <marcellorusso.x@gmail.com>
2024-05-24 10:02:19 +0200

    aggiunta sigmoide

--- uninaannpy/activation_functions.py
+++ uninaannpy/activation_functions.py
@@ -36,6 +36,24 @@ def tanh(x, der=False):
         return 1 - y**2  # Derivata della tangente iperbolica
 
 
+def sigmoid(x, der=False):
+    """
+    Calcola la funzione sigmoide o la sua derivata.
+
+    Args:
+        x (float): Il valore di input.
+        der (bool, optional): Se è False, calcola la funzione sigmoide, altrimenti calcola la derivata. Default è False.
+
+    Returns:
+        float: Se der=False, restituisce il valore della funzione sigmoide di x, altrimenti restituisce la sua derivata.
+    """
+    if not der:
+        return 1 / (1 + np.exp(-x))
+    else:
+        sig = 1 / (1 + np.exp(-x))
+        return sig * (1 - sig)  # Derivata della funzione sigmoide
+
+
 def relu(x, der=0):
     """
     Calcola la funzione ReLU o la sua derivata.
